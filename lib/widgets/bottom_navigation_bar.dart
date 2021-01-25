064bf45d45f9acc2a3fdf57df42082330ddf6eae
mfodat <69844255+mfodat@users.noreply.github.com>
2021-01-24 19:55:53 -0800

    custom bottom NavBar

--- lib/widgets/bottom_navigation_bar.dart
+++ lib/widgets/bottom_navigation_bar.dart
@@ -1,110 +1,70 @@
 import 'package:flutter/material.dart';
 import '../screens/show_history_screen.dart';
 
-const Color imageIconColor = Colors.blue;
-  const Color barItemBGColor =Colors.grey;
-const Color barBgColor =  Colors.red;
+
+const Color barItemBGColor = Colors.grey;
+const Color barBgColor = Colors.red;
 const double imageSize = 70;
 const double fontSize = 20;
 
 class MedicalPolicesBottomNavigationBar extends StatefulWidget {
-
   @override
-  _MedicalPolicesBottomNavigationBarState createState() => _MedicalPolicesBottomNavigationBarState();
+  _MedicalPolicesBottomNavigationBarState createState() =>
+      _MedicalPolicesBottomNavigationBarState();
 }
 
-class _MedicalPolicesBottomNavigationBarState extends State<MedicalPolicesBottomNavigationBar> {
-  int currentTab=0;
+class _MedicalPolicesBottomNavigationBarState
+    extends State<MedicalPolicesBottomNavigationBar> {
+  int currentTab = 0;
+  final notSelectedColor =  Colors.white ;
+  final selectedColor =  Colors.green ;
+  final numberOfIcon = 5;
+  final height = 60.0;
+  var screenSize ;
+  int _selectedIndex = 3;
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            activeIcon: ImageIcon(
-              AssetImage("images/group.png"),
-              color: Colors.deepPurple,
-              size: imageSize,
+       screenSize = MediaQuery.of(context).size;
 
-            ),
-            icon: ImageIcon(
-              AssetImage("images/group.png"),
-              color: Colors.green,
-              size: imageSize,
-            ),
-            // label: 'telemedicine',
-            title: Text(
-              'Login',
-              style: TextStyle(fontSize: fontSize, color: Colors.black),
-            ),
 
-            backgroundColor: barItemBGColor,
-          ),
-          BottomNavigationBarItem(
+    return Row(
+      children: [
+        buildNavBarItem(
+          iconData: Icons.home,
+          index: 0,
+        ), buildNavBarItem(
+          iconData: Icons.card_giftcard,
+          index: 1,
+        ), buildNavBarItem(
+          iconData: Icons.camera,
+          index: 2,
+        ), buildNavBarItem(
+          iconData: Icons.pie_chart,
+          index: 3,
+        ), buildNavBarItem(
+          iconData: Icons.person,
+          index: 4,
+        ),
+      ],
+    );
+  }
 
-            icon: ImageIcon(
-              AssetImage("images/request.png"),
-              color: imageIconColor,
-              size: imageSize,
-            ),
-            // label: 'Approvals',
-            title: Text(
-              'request',
-              style: TextStyle(fontSize: fontSize),
-            ),
-            backgroundColor: barItemBGColor,
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage("images/location.png"),
-              color: imageIconColor,
-              size: imageSize,
-            ),
-            //   label: 'dwaee',
-            title: Text(
-              'location',
-              style: TextStyle(fontSize: fontSize),
-            ),
-            backgroundColor: barItemBGColor,
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage("images/customer_service.png"),
-              color: imageIconColor,
-              size: imageSize,
-            ),
-            //  label: 'Eligibility',
-            title: Text(
-              'Contact us',
-              style: TextStyle(fontSize: fontSize),
-            ),
-            backgroundColor: barItemBGColor,
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage("images/cart.png"),
-              color: imageIconColor,
-              size: imageSize,
-            ),
-            // label: 'Benefits',
-            title: Text(
-              'cart',
-              style: TextStyle(fontSize: fontSize),
-            ),
-            backgroundColor: barItemBGColor,
-          ),
-        ],
-       backgroundColor: barBgColor,
-        type: BottomNavigationBarType.shifting,
-        currentIndex: currentTab,
-
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.deepPurple,
-        iconSize: 40,
-        onTap: (int index) {
-          setState(() {
-            currentTab = index;
-          });
-        },
-        elevation: 5);
+  Widget buildNavBarItem(
+      {IconData iconData,int index}) {
+    return GestureDetector(
+      onTap: (){ setState(() {
+        _selectedIndex = index;
+      });},
+      child: Container(
+        height: height,
+        width: screenSize.width/numberOfIcon,
+        decoration: BoxDecoration(
+          color: index == _selectedIndex ? selectedColor:notSelectedColor,
+        ),
+        child: Icon(
+          iconData,
+        ),
+      ),
+    );
   }
 }
